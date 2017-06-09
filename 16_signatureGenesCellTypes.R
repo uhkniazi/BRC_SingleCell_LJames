9d780090a8558331144442826099f56193f7bfe8
Umar Niazi <uhkniazi@gmail.com>
2017-06-09 14:26:31 +0100

    performed a random forest step. try with logistic regression

--- 16_signatureGenesCellTypes.R
+++ 16_signatureGenesCellTypes.R
@@ -7,30 +7,6 @@
 ## set variables and source libraries
 source('header.R')
 
-library(downloader)
-url = 'https://raw.githubusercontent.com/uhkniazi/CDiagnosticPlots/master/CDiagnosticPlots.R'
-download(url, 'CDiagnosticPlots.R')
-
-# load the required packages
-source('CDiagnosticPlots.R')
-# delete the file after source
-unlink('CDiagnosticPlots.R')
-
-######################### functions
-# Name: f_Plot3DPCA
-# Args: mComp = n X 3 matrix with first 3 components as the 3 column vectors
-#       color = colours for the points
-#       ... additional arguments to the plot function
-# Rets: none
-# Desc: takes the first 3 components and plots the data in a 3d plot
-f_Plot3DPCA = function(mComp, color, ...) {
-  x = mComp[,1]
-  y = mComp[,2]
-  z = mComp[,3]
-  if (!require(scatterplot3d)) stop('scatterplot3d library required')
-  scatterplot3d(x, y, z, color, ...)
-}
-
 ## connect to mysql database to get find path to appropriate file
 library('RMySQL')
 
@@ -42,11 +18,7 @@ q = paste0('select MetaFile.* from MetaFile
            where (MetaFile.idData = 13) AND (MetaFile.comment like "%NanoString%")')
 dfSample = dbGetQuery(db, q)
 dfSample
-q = paste0('select Sample.id as sid, Sample.group1, Sample.group2 as phenotype, Sample.title, File.* from Sample, File
-           where (Sample.idData = 13) AND (File.idSample = Sample.id)')
-dfSample.names = dbGetQuery(db, q)
-dim(dfSample.names)
-dfSample.names
+
 # close connection after getting data
 dbDisconnect(db)
 
@@ -57,15 +29,62 @@ load(n)
 ## make count matrix
 names(lNanoString)
 lNanoString$desc
-mCounts.raw = lNanoString$rawData$x
-mCounts.norm = lNanoString$normalizedData$normalized.data
-mCounts.raw = mCounts.raw[mCounts.raw$CodeClass == 'Endogenous', ]
-rownames(mCounts.raw) = mCounts.raw$Name
-mCounts.raw = as.matrix(mCounts.raw[,-c(1:3)])
 
+mCounts.norm = lNanoString$normalizedData$normalized.data
 mCounts.norm = mCounts.norm[mCounts.norm$Code.Class == 'Endogenous', ]
 rownames(mCounts.norm) = mCounts.norm$Name
-mCounts.norm = as.matrix(mCounts.norm[,-c(1:3)])
+mCounts = as.matrix(mCounts.norm[,-c(1:3)])
+
+dfData = data.frame(t(mCounts))
+## add the cell type id
+dfData$fCellID = factor(lNanoString$metaData$group2)
+
+library(randomForest)
+fit.rf = randomForest(fCellID ~ ., data=dfData)
+
+# get variables importance
+varImpPlot(fit.rf)
+dfRF = data.frame(importance(fit.rf))
+head(dfRF)
+ivScore = dfRF$MeanDecreaseGini
+names(ivScore) = rownames(dfRF)
+ivScore = sort(ivScore, decreasing = T)
+ivScore
+summary(ivScore)
+f = quantile(ivScore, 0.90)
+table(ivScore >= f)
+ivScore = ivScore[ivScore >= f]
+tail(ivScore)
+## keep the top names
+## for some reason the - in the name is replaced by a .
+## replace those first
+cvTopGenes = gsub('\\.', '-', names(ivScore))
+table(cvTopGenes %in% rownames(mCounts))
+
+## find correlated variables
+mCounts = mCounts[cvTopGenes,]
+mCor = cor(t(mCounts), use="na.or.complete")
+library(caret)
+### find the columns that are correlated and should be removed
+n = findCorrelation((mCor), cutoff = 0.7, names=T)
+data.frame(n)
+sapply(n, function(x) {
+  (abs(mCor[,x]) >= 0.7)
+})
+i = which(cvTopGenes %in% n)
+cvTopGenes.cor = cvTopGenes[-i]
+
+mCounts = mCounts[cvTopGenes.cor,]
+
+dfData = data.frame(scale(t(mCounts)))
+## add the cell type id
+dfData$fCellID = factor(lNanoString$metaData$group2)
+
+library(MASS)
+
+fit.lda = lda(fCellID ~ ., data=dfData)
+
+
 
 dfSample.names = lNanoString$metaData
 identical(dfSample.names$title, colnames(mCounts.norm))
